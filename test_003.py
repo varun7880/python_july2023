f48696565b8c93df7150ebd3e737d4011827269c
varun <varunnayar11@gmail.com>
2023-08-01 22:09:18 +0530

    first commit

---
+++ test_003.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+class Test_Credence_003:
+
+    def test_mul_004(self):
+        a = 3
+        b = 7
+        mul = a * b
+        print("Mul of a and b is :" + str(mul))
+        if mul == 21:
+            assert True
+        else:
+            assert False
+    @pytest.mark.xfail
+    def test_sub_005(self):
+        a = 12
+        b = 7
+        sub = a - b
+        print("Subtraction of a from b is :" + str(sub))
+        if sub == 5:
+            assert True
+        else:
+            assert False
